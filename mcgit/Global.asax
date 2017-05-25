7616fa421357455a491ed513ad225211eedcbce5
manishchopra@myself.com <manishchopra@myself.com>
2017-05-25 19:25:22 +0100

    Add project files.

---
+++ mcgit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mcgit.MvcApplication" Language="C#" %>
---
+++ mcgit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mcgit.Startup))]
+namespace mcgit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
