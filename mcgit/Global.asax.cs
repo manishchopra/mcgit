95933a595dbed4b3bec8055db09377ba371e1e59
manishchopra@myself.com <manishchopra@myself.com>
2017-05-25 19:28:13 +0100

    test

--- mcgit/Global.asax.cs
+++ mcgit/Global.asax.cs
@@ -16,6 +16,6 @@ namespace mcgit
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        } 
+        }         
     }
 }
